0cea8f4cb255fbaf851eff414fe3e274eb35f4f1
Anyelser <anyelserperez@gmail.com>
2022-10-05 19:20:25 -0400

    d

--- aplicaciones/juegos/api/api.py
+++ aplicaciones/juegos/api/api.py
@@ -1,11 +1,22 @@
+from urllib import request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from aplicaciones.juegos.models import Jugada
 from aplicaciones.juegos.api.serializers import JugadaSerializer
 
-class JugadaApiView(APIView):
+@api_view(['GET'])
+def jugada_api_view(request):
+
+    if request.method == 'GET':
+        jugadas = Jugada.objects.all()
+        jugadas_serializer = JugadaSerializer(jugadas,many=True)
+        return Response(jugadas_serializer.data)
+
+
+"""class JugadaApiView(APIView):
 
     def get(self, request):
         jugadas = Jugada.objects.all()
         jugadas_serializer = JugadaSerializer(jugadas,many=True)
-        return Response(jugadas_serializer.data)
+        return Response(jugadas_serializer.data)"""
--- aplicaciones/juegos/api/urls.py
+++ aplicaciones/juegos/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from aplicaciones.juegos.api.api import JugadaApiView
+from aplicaciones.juegos.api.api import jugada_api_view# JugadaApiView
 
 urlpatterns = [
-    path('jugada/',JugadaApiView.as_view(),name='jugadaApi')
+    path('jugada2/',jugada_api_view,name='jugadaApi2')
+    #path('jugada/',JugadaApiView.as_view(),name='jugadaApi')
 ]
