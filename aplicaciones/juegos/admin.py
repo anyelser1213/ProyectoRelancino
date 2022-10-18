c5295699304cf6953d647c359338226107599009
Parley Sport <parleysport@gmail.com>
2022-10-18 13:07:34 -0400

    agregando cambios

--- aplicaciones/juegos/admin.py
+++ aplicaciones/juegos/admin.py
@@ -4,7 +4,73 @@ from .models import Jugada, TipoJugadas
 
 # Register your models here.
 
+#Esta es otra forma de registrar en el admin
+@admin.register(TipoJugadas)
+class TipoJugadasAdmin(admin.ModelAdmin):
+
+    #readonly_fields = ('fecha_cierre',)
+
+    #Para indicarle al admin que campos queremos mostrar
+    list_display = ('id','nombre','cantidad_digitos','estado_jugada','hora_inicio','hora_cierre','fecha_cierre')#,'fecha_cierre'
+
+    #Para indicar que se va a visualizar como vinculo
+    list_display_links = ('nombre',)
+
+    #Para indicar que campos seran editables
+    list_editable = ('estado_jugada','hora_inicio','hora_cierre','fecha_cierre')#,'fecha_cierre'
+    ordering = ('cantidad_digitos','nombre',)
+
+"""
+
+    ordering = ('username',)
+    
+    #Aqui es cuando se va a editar
+    fieldsets = (
+        #Aqui es para editar
+        ("Informacion Esencial", {'fields': ('username', 'password')}),
+        ("Permisologia", {
+            'classes': ('wide',),
+            'fields': ('is_superuser','admin','activo','groups','user_permissions'),
+        }),
+    )
+
+    #Aqui es cuando se esta creando
+    add_fieldsets = (
+        ("Informacion Obligatoria", {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+        ("Informacion Importante", {
+            'classes': ('wide',),
+            'fields': ('activo', 'is_superuser','admin', 'cedula','telefono'),
+            #'fields': ('activo', 'is_superuser','admin', 'cedula','plan_elegido','rol','telefono'),
+        }),
+        ("Permisologia", {
+            'classes': ('wide',),
+            'fields': ('groups','user_permissions',),
+        }),
+    )
 
 
-admin.site.register(TipoJugadas)
+    #Para indicarle al admin que campos queremos mostrar
+    list_display = ('id','username','is_superuser','admin','activo','fecha_creacion','ultimo_ingreso')
+    #list_display = ('username', 'email','is_superuser','admin','rol','plan_elegido')
+    list_filter = ('username','activo')
+    
+    #Para especificar que campos van a efectuar la busqueda
+    search_fields = ('username', 'nombres', 'apellidos')
+    filter_horizontal = ()
+"""
+
+
+
+
+
+
+#Aqui registramos los elementos para que aparezcan en el admin de django(esta es otra forma)
+#admin.site.register(Usuarios, UserAdmin)
+
+
+
+#admin.site.register(TipoJugadas)
 admin.site.register(Jugada)
--- aplicaciones/juegos/models.py
+++ aplicaciones/juegos/models.py
@@ -1,14 +1,31 @@
-from contextlib import nullcontext
+
+from django.utils import timezone
+from django.utils.timezone import now
 from django.db import models
 from aplicaciones.usuarios.models import Usuarios
+
+import datetime
 
 # Create your models here.
 
 class TipoJugadas(models.Model):
 
+    """Modelo para registrar hora de inicio y cierre"""
+    day  = timezone.now()
+    hour = timezone.now()
+    #formatedHour = hour.strftime("%Y/%m/%d %H:%M:%S")
+    #formatedDay  = day.strftime("%Y/%m/%d")
+    #formatedHour = hour.strftime("%H:%M:%S")
+
     nombre = models.CharField(max_length=30,unique=True)
     cantidad_digitos = models.CharField(max_length=30)
     estado_jugada = models.BooleanField(default=False)
+    hora_inicio = models.TimeField(default=now, blank=False,null=False)#Solo hora
+    hora_cierre = models.TimeField(default=now, blank=True,null=True)#Solo hora
+    fecha_cierre = models.DateField(auto_now_add=False,auto_now=False,blank=True) #Solo fecha
+
+    #fecha_cierre = models.CharField(max_length=50, default=formatedDay)
+    #fecha_creacion2 = models.DateTimeField(default=now) # fecha y hora
     monto_jugada = models.FloatField(default=0)
     cantidad_maxima_repeticion = models.PositiveIntegerField(default=0)
 
