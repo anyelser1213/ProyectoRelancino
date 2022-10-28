bc44c6bf20f80d07480da0ef6c7d5cfc1fdf4922
Anyelser <anyelserperez@gmail.com>
2022-10-28 01:07:15 -0400

    modificando login

--- aplicaciones/fondo_imagenes/models.py
+++ aplicaciones/fondo_imagenes/models.py
@@ -1,20 +1,22 @@
+import os
 import shutil#libreria para borrar carpetas esten o no llenas
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
 
-def user_directory_path(instance, filename):
+#def fondo_login(instance, filename):
   
-    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
-    return 'img/fondos/{0}/{1}'.format(instance.nombre, filename)
+#    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
+#    return 'img/fondos/{0}/{1}'.format(instance.nombre, filename)
 
 
 class Login(models.Model):
 
-    fondo_login = models.CharField(max_length=200,unique=True)
-    imagen = models.ImageField(upload_to=user_directory_path,blank=True, null=True)
-    #imagen_logo = models.ImageField(upload_to='img/fondos/imagen_logo/',blank=True, null=True)
+    #fondo_login = models.ImageField(upload_to="login/imagenes/",blank=True, null=True)
+    fondo_login = models.ImageField(upload_to="login/imagenes/fondo",blank=True, null=True)
+    logo_login = models.ImageField(upload_to='login/imagenes/logo',blank=True, null=True)
     
     
 
@@ -26,13 +28,13 @@ class Login(models.Model):
 
     
     def __str__(self):
-        return str(self.nombre)
+        return "Fondo: "+str(self.fondo_login.name)+" ----- Logo: "+str(self.logo_login.name)
 
     def save(self, *args, **kwargs): 
 
         print("FONDOS, metodo save") 
-        print(self.imagen)
-        print("con nombre: ",self.imagen.name)
+        print(self.fondo_login.name)
+        print("con nombre: ",self.fondo_login.name)
         
         #self.video.name = self.categoria,"/",self.mes
         #aux = os.path.join(self.categoria,self.mes,self.video.name)
@@ -44,11 +46,13 @@ class Login(models.Model):
             
         try:
             #Para borrar el directorio y todo lo que haya dentro
-            ruta = os.path.join(settings.MEDIA_ROOT,'img','fondos',self.nombre)
+            #ruta = os.path.join(settings.MEDIA_ROOT,'login','imagenes',self.imagen)
             
             #Con esto borramos carpetas
-            shutil.rmtree(ruta)
-            print(ruta)
+            #shutil.rmtree(ruta)
+            #print(ruta)
+
+            pass
 
         except OSError as e:
 
@@ -64,10 +68,10 @@ class Login(models.Model):
 
         #indice_final = 
         #self.imagen.name = 'fondos'+str(self.nombre,self.imagen.name)
-        print(self.imagen)
+        print(self.fondo_login)
         #print("prueba: ",self.video.name[:self.video.name.index('.')])
         
         #self.nombre = self.video.name #[:self.video.name.index('.')] #Guardamos el nombre del video
         #self.video.name = os.path.join(str(self.categoria),str(self.subcategoria),self.video.name)#Guardamos la ruta
         #return False
-        super(Fondos, self).save(*args, **kwargs)
+        super(Login, self).save(*args, **kwargs)
