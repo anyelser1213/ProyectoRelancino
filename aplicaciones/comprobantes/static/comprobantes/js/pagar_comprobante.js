8eda75e68f82c6623d818f05df220a2bb700f454
anyelser <anyelserperez@gmail.com>
2022-11-16 16:00:40 -0400

    implementando logica de sweetaler en proyecto

--- aplicaciones/comprobantes/static/comprobantes/js/pagar_comprobante.js
+++ aplicaciones/comprobantes/static/comprobantes/js/pagar_comprobante.js
@@ -6,9 +6,33 @@
 function Pagar_Comprobante(tipo){
 
 
-    Swal.fire("Esta es una alerta dentro de js y tiene un id "+tipo.id);
+
+    Swal.fire({
+        title: 'Confirmar Pago de Comprobante: '+tipo.dataset.comprobante,
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'Confirmar',
+        denyButtonText: `Cancelar`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
 
 
 
-    
+
+          Swal.fire('Comprobante pagado!', '', 'success')
+
+
+
+        } else if (result.isDenied) {
+
+
+
+          Swal.fire('Cancelado', '', 'error')
+        }
+      })
+
+
+
+
 }
