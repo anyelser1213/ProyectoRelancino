a265e2a00cd087fffb107d773a8683530b93f8f0
anyelser <anyelserperez@gmail.com>
2022-12-02 14:53:23 -0400

    cambios finalizados

--- aplicaciones/juegos/templatetags/tag_permisos_tipos_jugadas.py
+++ aplicaciones/juegos/templatetags/tag_permisos_tipos_jugadas.py
@@ -11,13 +11,13 @@ register = template.Library()
 def tipos_permisos(usuario,tipo): # Only one argument.
     
     usuario_actual = Usuarios.objects.get(username=str(usuario))
-    #print("El Usuario es: ",usuario_actual.get_all_permissions())
+    print("El Usuario es: ",usuario_actual.get_all_permissions())
 
     if usuario_actual.has_perm('juegos.'+tipo):
-        #print("SI TIENE EL PERMISO: "+tipo)
+        print("SI TIENE EL PERMISO: "+tipo)
         return True
     else:
-        #print("NO TIENE EL PERMISO")
+        print("NO TIENE EL PERMISO")
         return False
         #return Video.objects.all()
     
